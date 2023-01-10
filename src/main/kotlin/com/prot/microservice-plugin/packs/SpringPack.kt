35fe1c190959407c3c1aee21d8b5be2fac8aeda9
muratslavich <slavichmo@gmail.com>
2023-01-10 03:58:23 +0300

    fix build

--- src/main/kotlin/com/prot/microservice-plugin/packs/SpringPack.kt
+++ src/main/kotlin/com/prot/microservice-plugin/packs/SpringPack.kt
@@ -1,7 +1,6 @@
 package com.prot.`microservice-plugin`.packs
 
 import com.prot.`microservice-plugin`.*
-import com.prot.microservice.plugin.*
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.artifacts.dsl.DependencyHandler
--- src/test/resources/build.gradle.kts
+++ src/test/resources/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("microservice-plugin")
+    id("com.prot.microservice-plugin")
 }
 
 microservice {
